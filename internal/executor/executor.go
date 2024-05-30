2a82e6a62c9d5fa3cee46b96b46b56a45887e55d
Vitor Lobo Ramos <lobocode@gmail.com>
2024-05-30 11:09:08 -0300

    add scan report for dryrun

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -3,9 +3,12 @@ package executor
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/olanta/olanta/scanner/internal/models"
 	"github.com/olanta/olanta/scanner/internal/scanner"
+	"github.com/olanta/olanta/scanner/internal/utils"
 )
 
 func ExecuteScan(language, path string) []models.Issue {
@@ -20,8 +23,27 @@ func ExecuteScan(language, path string) []models.Issue {
 		issues = s.Scan(path)
 	default:
 		fmt.Printf("Unsupported language: %s\n", language)
-		os.Exit(1)
 	}
 
 	return issues
+}
+
+func GenerateHTMLReport(issues []models.Issue, path string) {
+	report := utils.CreateHTMLReport(issues)
+	reportPath := filepath.Join(path, "scan_report_"+time.Now().Format("20060102150405")+".html")
+
+	file, err := os.Create(reportPath)
+	if err != nil {
+		fmt.Printf("Error creating report file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(report)
+	if err != nil {
+		fmt.Printf("Error writing to report file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Report generated at: %s\n", reportPath)
 }
